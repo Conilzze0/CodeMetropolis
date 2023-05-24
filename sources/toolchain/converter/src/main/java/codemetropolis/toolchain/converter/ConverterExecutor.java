0fd08f6bd58c477a6dc1d9b783ce7509011f1d50
h254209 <h254209@stud.u-szeged.hu>
2023-05-24 15:12:28 +0200

    test precommit

--- sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/ConverterExecutor.java
+++ sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/ConverterExecutor.java
@@ -12,44 +12,43 @@ import codemetropolis.toolchain.commons.util.Resources;
 import codemetropolis.toolchain.converter.control.ConverterLoader;
 
 public class ConverterExecutor extends AbstractExecutor {
-	
-	@Override
-	public boolean execute(ExecutorArgs args) {
-		ConverterExecutorArgs converterArgs = (ConverterExecutorArgs) args;
-		System.out.println("ConverterExecutorArgs converterArgs = (ConverterExecutorArgs) argsConverterExecutorArgs converterArgs = (ConverterExecutorArgs) argsConverterExecutorArgs converterArgs = (ConverterExecutorArgs) argsConverterExecutorArgs converterArgs = (ConverterExecutorArgs) argsConverterExecutorArgs converterArgs = (ConverterExecutorArgs) argsConverterExecutorArgs converterArgs = (ConverterExecutorArgs) args");
-		CdfConverter converter = ConverterLoader.load(converterArgs.getType(), converterArgs.getParams());
-		converter.addConverterEventListener(new ConverterEventListener() {
-			
-			@Override
-			public void onConverterEvent(ConverterEvent event) {
-				print(event.getMessage());
-			}
-			
-		});
-		
-		print(Resources.get("converting_to_cdf"));
-		CdfTree cdfTree = null;
-		try {
-			cdfTree = converter.createElements(converterArgs.getSource());
-		} catch (CodeMetropolisException e) {
-			printError(e, e.getMessage());
-			return false;
-		} catch (Exception e) {
-			printError(e, Resources.get("converter_error"));
-			return false;
-		}
-		print(Resources.get("converting_to_cdf_done"));
-		
-		print(Resources.get("printing_cdf"));
-		try {
-			cdfTree.writeToFile(converterArgs.getOutputFile());
-		} catch (CdfWriterException e) {
-			printError(e, Resources.get("cdf_writer_error"));
-			return false;
-		}
-		print(Resources.get("printing_cdf_done"));
-		
-		return true;
-	}
+
+    @Override
+    public boolean execute(ExecutorArgs args) {
+        ConverterExecutorArgs converterArgs = (ConverterExecutorArgs) args;
+        CdfConverter converter = ConverterLoader.load(converterArgs.getType(), converterArgs.getParams());
+        converter.addConverterEventListener(new ConverterEventListener() {
+
+            @Override
+            public void onConverterEvent(ConverterEvent event) {
+                print(event.getMessage());
+            }
+
+        });
+
+        print(Resources.get("converting_to_cdf"));
+        CdfTree cdfTree = null;
+        try {
+            cdfTree = converter.createElements(converterArgs.getSource());
+        } catch (CodeMetropolisException e) {
+            printError(e, e.getMessage());
+            return false;
+        } catch (Exception e) {
+            printError(e, Resources.get("converter_error"));
+            return false;
+        }
+        print(Resources.get("converting_to_cdf_done"));
+
+        print(Resources.get("printing_cdf"));
+        try {
+            cdfTree.writeToFile(converterArgs.getOutputFile());
+        } catch (CdfWriterException e) {
+            printError(e, Resources.get("cdf_writer_error"));
+            return false;
+        }
+        print(Resources.get("printing_cdf_done"));
+
+        return true;
+    }
 
 }
--- sources/toolchain/converter/src/test/java/codemetropolis/toolchain/converter/gitstat/GetPropertiesFromDatFilesTest.java
+++ sources/toolchain/converter/src/test/java/codemetropolis/toolchain/converter/gitstat/GetPropertiesFromDatFilesTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 class GetPropertiesFromDatFilesTest {
-    List<CdfProperty> properties;
+    private List<CdfProperty> properties;
 
 
     @Test
